8825330af0ca94269bb1dc48418ea9115c3ff7ae
Markin Vlad <inverted.crow@rambler.ru>
2013-04-17 09:28:18 +0300

    update
    ctrl+shift+y was working full
    in next step will be adding bookmark from this window

---
+++ research-plugin/js/content.js
@@ -0,0 +1,7 @@
+﻿window.addEventListener("keydown", function(event) {
+  var modifier = event.ctrlKey || event.metaKey;
+  if (modifier && event.shiftKey && event.keyCode == 89) {
+	window.open(chrome.extension.getURL("/html/window.html"), 'title', 'width=300, height=300, left=450, top=60');
+	
+  }
+});
