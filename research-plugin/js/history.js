41e0eff012fd6ee485172bb7756b755664433e1f
ImmortalJoker <krasnovegorinc@gmail.com>
2013-04-25 22:32:57 +0300

    history.html show all img

--- research-plugin/js/history.js
+++ research-plugin/js/history.js
@@ -3,11 +3,17 @@ function addRow(date, icon, url, title) {
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
-    var url_length = url.length;
-    var icon_url = icon !== undefined ? url.substring(0, url_length) + icon : "../img/16.png";
-    //var icon_url = url.substring(0, url_length) + icon;
+    if (icon === undefined) {
+        var icon_url = "../img/16.png";
+    }
+    else if (icon.substring(0, 1) !== '/') {
+        var icon_url = icon;
+    }
+    else {
+        var icon_url = url.substring(0, url.length) + icon;
+    }
     cell1.innerHTML = date;
-    cell2.innerHTML = ' <img src="'+ icon_url +'" height="16"> ' + '<a href="' + url + '">' + title + '</a>';
+    cell2.innerHTML = ' <img src="' + icon_url + '" height="16"> ' + '<a href="' + url + '">' + title + '</a>';
 }
 
 window.onload = function () {
@@ -15,7 +21,7 @@ window.onload = function () {
     var temp = localStorage.getItem('localHistory');
     localHistory = JSON.parse(temp);
 
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < 20; i++) {
         var historyObject = localHistory[i];
         addRow(historyObject.date, historyObject.icon, historyObject.url, historyObject.title);
     }
