2e3dc6e047a3957abd0fc9d4508b2b4d55e75e89
Markin Vlad <inverted.crow@rambler.ru>
2013-04-27 23:50:11 +0300

    working, not all
    need to make normal massage from windows.js to background.js

--- research-plugin/js/background.js
+++ research-plugin/js/background.js
@@ -1,36 +1,38 @@
 ﻿var windowObj ={},
     runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
         'runtime' : 'extension';
-
-chrome[runtimeOrExtension].onMessage.addListener(
+//listen massage from content.js
+chrome.extension.onMessage.addListener(
     function (request, sender, sendResponse) {
         sendResponse({farewell: "OK!"});
         windowObj.title = request.page_data.title;
         windowObj.url = request.page_data.url;
         windowObj.icon = request.page_data.icon;
         windowObj.date = request.page_data.date;
-        addToHistory(windowObj);
-    });
-
-function addToHistory(object_value) {
-    var tempArr = new Array();
-    if (localStorage.localHistory)
-        tempArr = JSON.parse(localStorage.localHistory);
-    tempArr.push(object_value);
-   // alert('In Add to history' + JSON.stringify(object_value));
-    localStorage.localHistory = JSON.stringify(tempArr);
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button').addEventListener('click', addToHistory);
+        alert('adding to session history');
+        var tempArr = new Array();
+        if (sessionStorage.localHistory)
+            tempArr = JSON.parse(sessionStorage.localHistory);
+        tempArr.push(windowObj);
+        // alert('In Add to history' + JSON.stringify(object_value));
+        sessionStorage.localHistory = JSON.stringify(tempArr);
+        //send massage to window.js
+        chrome.runtime.sendMessage(windowObj, function (response) {
+            console.log(response.farewell);
+            alert(sessionStorage.localHistory)
+        });
 });
 
+//listen massage from window.js
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        sendResponse({farewell: "OK!"});
+        alert('added to local history');
+        var tempArr = new Array();
+        if (localStorage.localHistory)
+            tempArr = JSON.parse(localStorage.localHistory);
+        tempArr.push(request);
+        // alert('In Add to history' + JSON.stringify(object_value));
+        localStorage.localHistory = JSON.stringify(tempArr);
+});
 
-window.onload = function(){
-    var title = document.getElementById('title');
-    var url = document.getElementById('url');
-    var icon = document.getElementById('icon');
-    title.value = windowObj.title;
-    url.value = windowObj.url;
-    icon.value = windowObj.icon;
-}
--- research-plugin/js/windows.js
+++ research-plugin/js/windows.js
@@ -2,33 +2,31 @@ var runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
         'runtime' : 'extension',
     windowObj = {};
 
-chrome[runtimeOrExtension].onMessage.addListener(
-    function (request, sender, sendResponse) {
-        sendResponse({farewell: "OK!"});
-        windowObj.title = request.page_data.title;
-        windowObj.url = request.page_data.url;
-        windowObj.icon = request.page_data.icon;
-        windowObj.date = request.page_data.date;
-        alert(windowObj);
-    });
-
-
-function addToHistory() {
-    var tempArr = new Array();
-    if (localStorage.localHistory)
-        tempArr = JSON.parse(localStorage.localHistory);
-    tempArr.push(windowObj);
-    alert(JSON.stringify(windowObj));
-    localStorage.localHistory = JSON.stringify(tempArr);
-}
-document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button').addEventListener('click', addToHistory);
-});
 
 window.onload = function () {
-    var title = document.getElementById('title');
-    var url = document.getElementById('url');
-    var icon = document.getElementById('icon');
-    title.value = windowObj.title;
-    url.value = windowObj.url;
-};
+    //listen massage from background.js
+    chrome.runtime.onMessage.addListener(
+        function (request, sender, sendResponse) {
+            sendResponse({farewell: "OK!"});
+            var title = document.getElementById('title');
+            var url = document.getElementById('url');
+            var icon = document.getElementById('icon');
+            title.value = request.title;
+            url.value = request.url;
+            icon.value = request.icon;
+            document.addEventListener('DOMContentLoaded', function () {
+                windowObj.title = document.getElementById('title').value;
+                windowObj.url = document.getElementById('url').value;
+                windowObj.icon = request.icon;
+                windowObj.date = request.date;
+                function addToHistory() {
+                    chrome.extention.sendMessage(windowObj, function (response) {
+                        console.log(response.farewell);
+                        alert(sessionStorage.localHistory)
+                    })
+                }
+
+                document.querySelector('button').addEventListener('click', addToHistory);
+        });})
+}
+
