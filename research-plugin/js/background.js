c4c621b86891d7a60b516976d961ca0fbf95806a
Markin Vlad <inverted.crow@rambler.ru>
2013-05-01 13:47:53 +0300

    Little changes
    now this alert wery need -_-

--- research-plugin/js/background.js
+++ research-plugin/js/background.js
@@ -1,6 +1,7 @@
 ﻿var windowObj = {},
     runtimeOrExtension = chrome.runtime && chrome.runtime.sendMessage ?
         'runtime' : 'extension';
+
 //listen massage from content.js
 chrome.extension.onMessage.addListener(
     function (request, sender, sendResponse) {
@@ -9,6 +10,7 @@ chrome.extension.onMessage.addListener(
         windowObj.url = request.page_data.url;
         windowObj.icon = request.page_data.icon;
         windowObj.date = request.page_data.date;
+        alert('Edit title, if You need.');
         var tempArr = new Array();
         if (sessionStorage.localHistory)
             tempArr = JSON.parse(sessionStorage.localHistory);
@@ -18,7 +20,6 @@ chrome.extension.onMessage.addListener(
         //send massage to window.js
         chrome.runtime.sendMessage(windowObj, function (response) {
             console.log(response.farewell);
-            alert(sessionStorage.localHistory)
         });
     });
 
